cdf019fadae17007f0c33a7d0e1091074e7f2d15
nancyzt <tzhao018@uottawa.ca>
2021-11-21 23:02:31 -0500

    Added files

---
+++ doc2vec_reranking.py
@@ -0,0 +1,8 @@
+import json
+def doc2vec_reranking(file1="docID_1000.txt", file2="wordlist_1000.txt"):
+    # Using readlines()
+    docID_file = open(file1).read()
+    wordlist_file = open(file2)
+
+    return docID_file
+
