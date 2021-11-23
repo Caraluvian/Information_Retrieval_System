c246ca70740bc4f386d3e820c8bf0b66f459188f
nancyzt <tzhao018@uottawa.ca>
2021-11-22 22:52:11 -0500

    Updated

--- doc2vec_reranking.py
+++ doc2vec_reranking.py
@@ -1,8 +1,10 @@
 import json
 def doc2vec_reranking(file1="docID_1000.txt", file2="wordlist_1000.txt"):
     # Using readlines()
-    docID_file = open(file1).read()
-    wordlist_file = open(file2)
+    docID_file = open(file1, "r", encoding='UTF-8-sig').read()
+    wordlist_file = open(file2, "r", encoding='UTF-8-sig').read()
+    docID_ls = [i.strip()[1:-1] for i in docID_file[1:-1].split(',')]
+    word_ls = [i.strip()[1:-1] for i in wordlist_file[1:-1].split(',')]
 
-    return docID_file
+    return word_ls
 
--- test_query.py
+++ test_query.py
@@ -1,8 +1,22 @@
 import os
 from bs4 import BeautifulSoup
 import retrieval_ranking as rr
+import tweet_tokenizer as tweet
+
+
+def dict_convertor(trec_ls=tweet.tweet_tokenizer()):
+
+    # Create a doc dictionary with docID as key and word list as value
+    doc_dict = {}
+    for i in range(len(trec_ls)):
+        docID = trec_ls[i][0]
+        doc_dict[docID] = trec_ls[i][1:-1]
+    return doc_dict
 
 def test_query(file="topics_MB1-49.txt"):
+    # Create a doc dictionary with docID as key and word list as value
+    doc_dict = dict_convertor()
+    
     # Read test_query file
     test_file = open(file, "r").read()
     soup = BeautifulSoup(test_file, "html.parser")
@@ -19,19 +33,28 @@ def test_query(file="topics_MB1-49.txt"):
         title_ls.append(title)
 
     # Search 49 test queries, compute similarity and get top1000 ranked docs
-    if os.path.exists("Results.txt"):
-        os.remove("Results.txt")
+    docID_ls = []
+    word_ls = []
+    num = 0
+    #if os.path.exists("Results.txt"):
+    #   os.remove("Results.txt")
     print("Starting inserting top1000 ranked docs...")
     for x in range(len(title_ls)):
         similarity_sorted, ranked_docs = rr.retrieval_ranking(title_ls[x])
         # Write top1000 ranked docs into Results.txt
         limit = 1
         for y in range(len(ranked_docs)):
-            
-            with open("Results.txt", "a") as file:
-                file.write(f"{topicId_ls[x]} Q0 {ranked_docs[y]} {limit} {int(similarity_sorted[y][1]*1000)/1000} myRun\n")
+            num += 1
+            print(f"Writing document no.{num}")
+            with open("docID_1000.txt", "a+", encoding='UTF-8-sig') as file1:
+                file1.write(f"{ranked_docs[y]}\n")
+            with open("wordlist_1000.txt", "a+", encoding='UTF-8-sig') as file2:
+                file2.write(f"{doc_dict[ranked_docs[y]]}\n")
+            #docID_ls.append(ranked_docs[y])
+            #word_ls.append(doc_dict[ranked_docs[y]])
             if limit == 1000:
                 break
             limit += 1
 
-    print("File writing finished!")
+    #return docID_ls, word_ls
+    print("Finished!")
